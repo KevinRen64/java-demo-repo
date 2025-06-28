488491f0d22a5991e3780b9b8a47289fb437f309
KevinRen64 <147006477+KevinRen64@users.noreply.github.com>
2025-06-29 11:54:20 +1200

    version1

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Java CI/CD Pipeline!");
+        System.out.println("Hello from Java CI/CD Pipeline! version1");
     }
 }
